095e8a920aeb9cb5dc6ea49d0c109c2278477afd
Beto Zarzoza <betozarzoza@gmail.com>
2025-12-31 00:46:29 -0600

    add 2026 year

--- resources/views/zenix/form/add_maintenance_payment.blade.php
+++ resources/views/zenix/form/add_maintenance_payment.blade.php
@@ -59,7 +59,8 @@
 	                                    <option value="2022">2022</option>
 	                                    <option value="2023">2023</option>
 	                                    <option value="2024">2024</option>
-	                                    <option selected="selected" value="2025">2025</option>
+	                                    <option value="2025">2025</option>
+	                                    <option selected="selected" value="2026">2026</option>
 	                                </select>
 	                            </div>
 	                            <div class="mb-3">
